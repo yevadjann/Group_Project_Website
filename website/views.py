b2213cf78fb6a56513fe2ee4e40447b5774d3c50
yevadjann <evazembik@gmail.com>
2025-01-26 00:09:09 +0100

    main page was created

--- website/views.py
+++
@@ -1,9 +0,0 @@
-# Here we will be storing our standard routes
-# Blueprint is a bunch of URLs defined
-from flask import Blueprint
-
-views = Blueprint('views', __name__)
-
-@views.route('/')
-def home():
-    return "<h1>Quiz</h1"
