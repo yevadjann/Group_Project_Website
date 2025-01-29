3066dfb37d5453b3956d18ef1f1fcf287ab33fe7
gurowamuki <gurowamuki@gmail.com>
2025-01-29 20:23:07 +0100

    main page + buttons

--- quiz_website/app.py
+++ quiz_website/app.py
@@ -1,1 +1,19 @@
-print('HElloq world')
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
