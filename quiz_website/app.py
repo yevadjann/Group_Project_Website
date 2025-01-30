50c35ac5d85116c78ff2756c6e2ff2fcbd08d0dd
gurowamuki <gurowamuki@gmail.com>
2025-01-30 21:58:12 +0100

    updated buttons + db

--- quiz_website/app.py
+++ quiz_website/app.py
@@ -16,24 +16,39 @@ login_manager.login_view = 'login'
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
 
-    def __init__(self, username, email, password):
+    def __init__(self, username, email, password=None):
         self.username = username
         self.email = email
-        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        if password:
+            self.set_password(password)
 
+    def set_password(self, password):
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
     def check_password(self, password):
         return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
 
+class QuizAnswer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    question_id = db.Column(db.Integer, nullable=False)
+    answer = db.Column(db.String(100), nullable=False)
+    quiz_name = db.Column(db.String(100), nullable=False)
+
+    user = db.relationship('User', backref=db.backref('answers', lazy=True))
+
+
 with app.app_context():
     db.create_all()
 
+
+
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return User.query.get_or_404(int(user_id))
 
 @app.route('/')
 def index():
@@ -48,18 +63,16 @@ def register():
 
         if not username or not email or not password:
             return render_template('register.html', error="All fields are required.")
+        existing_user = User.query.filter((User.email == email) | (User.username == username)).first()
 
-        existing_user = User.query.filter_by(email=email).first()
         if existing_user:
             return render_template('register.html', error='Email already registered')
 
-        new_user = User(username = username, email = email, password = password)
+        new_user = User(username=username, email=email, password=password)
         db.session.add(new_user)
         db.session.commit()
 
         login_user(new_user)
-        session['username'] = new_user.username
-
         return redirect('/select_quiz')
 
     return render_template('register.html')
@@ -73,22 +86,50 @@ def login():
         if not email or not password:
             return render_template('login.html', error="Please fill in all fields.")
 
-
-        user = User.query.filter_by(email = email).first()
+        user = User.query.filter_by(email=email).first()
 
         if user and user.check_password(password):
             login_user(user)
-            session['username'] = user.username
-            return redirect('/select_quiz') #!!!!  return redirect(url_for('select_quiz'))
+            return redirect(url_for('select_quiz'))
         else:
-            return render_template('login.html', error = 'Invalid user')
+            return render_template('login.html', error='Invalid email or password')
 
     return render_template('login.html')
 
 @app.route('/select_quiz')
 @login_required
 def select_quiz():
-    return  render_template('select_quiz.html', username = current_user.username)
+    return  render_template('select_quiz.html', username=current_user.username)
+
+@app.route('/food_quiz')
+@login_required
+def food_quiz():
+    return render_template('food_quiz.html')
+
+@app.route('/music_quiz')
+@login_required
+def music_quiz():
+    return render_template('music_quiz.html')
+
+@app.route('/history_quiz')
+@login_required
+def history_quiz():
+    return render_template('history_quiz.html')
+
+@app.route('/travel_quiz')
+@login_required
+def travel_quiz():
+    return render_template('travel_quiz.html')
+
+@app.route('/psychology_quiz')
+@login_required
+def psychology_quiz():
+    return render_template('psychology_quiz.html')
+
+@app.route('/special_quiz')
+@login_required
+def special_quiz():
+    return render_template('special_quiz.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
